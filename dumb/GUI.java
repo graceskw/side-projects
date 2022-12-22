faf066eee4ad14c877a3c109e334db2338a724f1
graceskw <graceskw@connect.hku.hk>
2022-12-22 14:25:36 +0800

    finished
    - used gridbag layout in the end
    - fixed bug for randomising location of no button

--- dumb/GUI.java
+++ dumb/GUI.java
@@ -1,15 +1,17 @@
 import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 import java.lang.Math;
 
 public class GUI{
-    JFrame frame = new JFrame("Are you dumb?");
+    JFrame frame = new JFrame();
     JPanel panel = new JPanel();
+    JLabel label = new JLabel("Are you dumb?");
     JButton yes = new JButton("Yes");
     JButton no = new JButton("No");
 
@@ -18,14 +20,30 @@ public class GUI{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.add(panel);
-        panel.add(yes);
-        panel.add(no);
-        no.setLocation((int) Math.random()*frame.getWidth(), );
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 1;
+        c.gridy = 1;
+        c.gridwidth = 2;
+        panel.add(label, c);
+        yes.setSize(10,5);
+        c.gridx = 1;
+        c.gridy = 2;
+        c.gridwidth = 1;
+        panel.add(yes, c);
+        no.setSize(10,5);
+        c.gridx = 2;
+        c.gridy = 2;
+        c.gridwidth = 1;
+        panel.add(no, c);
+        yes.addActionListener(new YesButton());
+        no.addActionListener(new NoButton());
+       
     }
 
     public class NoButton implements ActionListener{
         public void actionPerformed(ActionEvent e){
-            no.setLocation((int) Math.random()*frame.getWidth(), (int) Math.random()*frame.getHeight());
+            no.setLocation((int) (Math.random()*frame.getWidth()), (int) (Math.random()*frame.getHeight()));
         }
     }
 
@@ -33,17 +51,12 @@ public class GUI{
         public void actionPerformed(ActionEvent e){
             yes.setVisible(false);
             no.setVisible(false);
-            JLabel label = new JLabel("Good to know :)))))))");
+            label.setText("Good to know :)))))))");
             panel.add(label);
-            // JDialog dialog = new JDialog(frame, "Good to know :)))))))");
         }
     }
 
     public static void main(String[] args){
         GUI gui = new GUI();
-        gui.panel.add(gui.yes);
-        gui.panel.add(gui.no);
-        gui.yes.addActionListener(gui.new YesButton());
-        gui.no.addActionListener(gui.new NoButton());
     }
 }

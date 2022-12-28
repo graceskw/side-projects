9473dcdc94415efeb4e974360be38b7908adeee7
graceskw <graceskw@connect.hku.hk>
2022-12-28 17:38:07 +0800

    ipa_flashcard
    - separated constructor and createGUI
    - added boolean lightMode to determine the colour scheme
    - added getImage to load the flashcards
    - attemped to draw the flashcards and a line on flashcardPanel but nothing showed

--- ipa_flashcard/GUI.java
+++ ipa_flashcard/GUI.java
@@ -8,6 +8,7 @@ import javax.swing.*;
 public class GUI {
     int mode;
     int symbolSet;
+    boolean lightMode = true;
 
     private JFrame frame;
     private JPanel flashcardPanel;
@@ -23,6 +24,9 @@ public class GUI {
     private JMenuItem restart;
     private JMenuItem quit;
     
+    Image[] vowelImages = new Image[28];
+    Image[] consonantImages = new Image[186];
+
     public GUI(){
         // to choose the mode
         String[] modeOptions = new String[]{"IPA to description", "Description to IPA"};
@@ -34,6 +38,16 @@ public class GUI {
         this.symbolSet = JOptionPane.showOptionDialog(null, "Please choose desired IPA symbol set:", "IPA symbol set",
         JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, symbolSetOptions, symbolSetOptions[0]);        // this.mode.showOptionDialog(null, "Please choose desired mode:",
         
+        getImage();
+        createGUI();
+        
+        this.frame.setVisible(true);
+        frame.repaint();
+        flashcardPanel.repaint();
+    }
+    
+    // create GUI
+    private void createGUI(){
         this.frame = new JFrame();
         // set frame title according to mode and symbol set
         if(mode == 0){ // IPA to description
@@ -60,53 +74,73 @@ public class GUI {
         this.menuBar.add(menu);
         this.frame.setJMenuBar(menuBar);
 
-// use gridbaglayout for the panels in the frame
         this.frame.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
+        // create flashcardPanel and add to frame
         this.flashcardPanel = new FlashcardPanel();
             c.gridx = 0;
             c.gridy = 0;
             c.gridwidth = 1;
             c.gridheight = 5;
-        this.frame.add(flashcardPanel);
+            // c.insets = new Insets(10, 10, 10, 10);
+        this.frame.add(flashcardPanel, c);
 
+        // create optionPanel and add to frame
         this.optionPanel = new OptionPanel();
             c.gridx = 0;
             c.gridy = 5;
             c.gridwidth = 1;
             c.gridheight = 3;
-        this.frame.add(optionPanel);
+        this.frame.add(optionPanel, c);
         
         // create buttonPanel and create+add confirm, cancel, and skip buttons
         this.buttonPanel = new JPanel();
-        this.buttonPanel.setLayout(new GridLayout(1, 3));
+        this.buttonPanel.setLayout(new GridLayout(1, 2));
         confirmButton = new JButton("Confirm");
-        cancelButton = new JButton("Cancel");
+        // cancelButton = new JButton("Cancel");
         skipButton = new JButton("Skip");
         this.buttonPanel.add(confirmButton);
-        this.buttonPanel.add(cancelButton);
+        // this.buttonPanel.add(cancelButton);
         this.buttonPanel.add(skipButton);
             c.gridx = 0;
             c.gridy = 8;
             c.gridwidth = 1;
             c.gridheight = 2;
-        this.frame.add(buttonPanel);
-        
-        
+            c.fill = GridBagConstraints.HORIZONTAL;
+        this.frame.add(buttonPanel, c);
 
-        this.frame.setVisible(true);
+    }
+
+    // get images of flashcards
+    private void getImage(){
+        // light mode
+        if(lightMode){
+            for(int i=1; i<=28; i++){
+                vowelImages[i-1] = new ImageIcon("ipa_symbols/vowel light2/vowel light_page-000"+i+".jpg").getImage();
+            }
+            // load consonant
+        }
+
     }
     
-    
-    
     public class FlashcardPanel extends JPanel{
-        public FlashcardPanel(){
-            flashcardPanel = new JPanel();
-            flashcardPanel.setLayout(new BorderLayout());
+        private int cardWidth = 297;
+        private int cardHeight = 210;
+
+// doesnt work, not even line is drawn
+        public void paintComponent(Graphics g) {   
             if(mode == 0){
                 // show IPA symbol
                 if(symbolSet == 0){
                     // show vowel symbol
+                    super.paintComponent(g);
+                    Graphics2D g2D = (Graphics2D) g;
+                    g2D.setColor(Color.black);
+                    // int h = this.getHeight();
+                    // int w = this.getWidth();
+        
+                    g2D.drawImage(vowelImages[0], 0, 0, cardWidth, cardHeight, this);
+                    g2D.drawLine(0, 0, this.getWidth(), this.getHeight());
                 }
                 else if(symbolSet == 1){
                     // show consonant symbol
@@ -134,7 +168,6 @@ public class GUI {
             }
         }
 
-        // paint component method
     }
     
     public class OptionPanel extends JPanel{
@@ -143,6 +176,22 @@ public class GUI {
                 // show description
                 if(symbolSet == 0){
                     // show vowel description
+                    setLayout(new GridLayout(1, 4));
+                    String[] s1 = {"high", "mid", "low"};
+                    JComboBox<String> tongueHeight = new JComboBox<>(s1);
+                    String[] s2 = {"front", "cental", "back"};
+                    JComboBox<String> backness = new JComboBox<>(s2);
+                    String[] s3 = {"tense", "lax"};
+                    JComboBox<String> tense = new JComboBox<>(s3);
+                    String[] s4 = {"unrounded", "rounded"};
+                    JComboBox<String> round = new JComboBox<>(s4);
+                    add(tongueHeight);
+                    add(backness);
+                    add(tense);
+                    add(round);
+                }
+                else if(symbolSet == 1){
+                    // show consonant description
                     setLayout(new GridLayout(1, 5));
                     String[] s1 = {"pulmonic", "non-pulmonic"};
                     JComboBox<String> airflow = new JComboBox<>(s1);
@@ -159,10 +208,6 @@ public class GUI {
                     add(centrality);
                     add(oral);
                     add(manner);
-                }
-                else if(symbolSet == 1){
-                    // show consonant description
-                    // to be implemented
                 }
                 else if(symbolSet == 2){
                     // show vowel and consonant description

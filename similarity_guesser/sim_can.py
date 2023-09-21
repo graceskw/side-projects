41316f0f7e9585cb8758a0aabcf0a3f049a40fbc
So Ki Wai Grace <98684001+graceskw@users.noreply.github.com>
2023-09-22 00:26:48 +0800

    Similarity game: feat: Cantonese version
    Changed all instructions to Cantonese

--- similarity_guesser/sim_can.py
+++ similarity_guesser/sim_can.py
@@ -2,8 +2,7 @@ from gensim.models import KeyedVectors
 import pandas as pd
 import random
 
-wv_from_text = KeyedVectors.load_word2vec_format("./toastynews.vec", binary=False)
-wv = wv_from_text
+wv = KeyedVectors.load_word2vec_format("./toastynews.vec", binary=False)
 
 pdFreq = pd.read_csv('Most Common Cantonese Words (Frequency List)_over 10.csv')
 freqList = pdFreq['Word'].tolist()
@@ -14,25 +13,22 @@ while(answer not in wv.key_to_index):
 
 print(answer)
 print(wv.most_similar(answer))
-wordList = []
 simList = []
 for word in wv.key_to_index:
     tmp = [word, wv.similarity(answer, word)]
     simList.append(tmp)    
       
 simList.sort(key=lambda l:l[1], reverse=True)
+wordList = [simList[x][0] for x in range(len(simList))]
 
-guess = input("Enter a word: ")
+guess = input("請輸入字詞: ")
 while(guess != answer):
     if(guess not in wv.key_to_index):
-        print("Not a valid word")
+        print("搵唔到呢個字/詞語 :(")
     else:
-        ranking = 0
-        for i in range(len(simList)):
-            if guess == simList[i][0]:
-                ranking = i+1
-        print(f"Your guess is: {guess}, it's ranking in terms of similarity to the answer is {ranking}")
+        ranking = wordList.index(guess)
+        print(f"你估咗: {guess}, 佢同答案嘅相似度爭{ranking}名")
     
-    guess = input("Enter a word: ")
+    guess = input("請輸入字詞: ")
 
-print(f"Congrats, You've guessed it! The answer is {answer}")
+print(f"恭喜晒, 你估中咗啦! 個答案係: {answer}")
